cd56fb01cfec879a2f4e2e4a12efae1d672b6558
ali00006 <hasanmonu116@gmail.com>
2025-10-19 07:38:22 +0530

    forbid set to fasle to run CI

--- playwright.config.js
+++ playwright.config.js
@@ -20,7 +20,12 @@ module.exports = defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  // forbidOnly: !!process.env.CI,
+
+  // This is generally not recommended as it could allow
+  // you to accidentally commit `.only`
+  forbidOnly: false,
+
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
